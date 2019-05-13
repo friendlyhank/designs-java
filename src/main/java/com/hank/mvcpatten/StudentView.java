7b672a1d640fa85880ce9712850d00135338d938
hank.com <hanguang8888@outlook.com>
2019-05-13 17:02:08 +0800

    Add files via upload

---
+++ src/main/java/com/hank/mvcpatten/StudentView.java
@@ -0,0 +1,12 @@
+package com.hank.mvcpatten;
+
+/**
+ * View静态页面
+ */
+public class StudentView {
+    public void printStudentDetails(String studentName,String studentRollNo){
+        System.out.println("Student: ");
+        System.out.println("Name: "+studentName);
+        System.out.println("Roll No: "+studentRollNo);
+    }
+}
